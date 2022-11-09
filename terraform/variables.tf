83eb119d5ceb85cfd2b5a38e1ab23da57ba95536
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-11-09 21:09:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
-variable "key_name" {
+/*
+  variable "key_name" {
   type        = string
   description = "The name for ssh key, used for aws_launch_configuration"
 }
@@ -7,3 +8,4 @@ variable "cluster_name" {
   type        = string
   description = "The name of AWS ECS cluster"
 }
+*/
