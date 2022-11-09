ac12a42d527b37b93f497f8ff9479f2ef8996957
gogiyahimanshi <104900471+gogiyahimanshi@users.noreply.github.com>
2022-11-09 20:58:31 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source         = "terraform-aws-modules/vpc/aws"
-  version        = "3.15.0"
+  version        = "3"
   name           = "test_ecs_provisioning"
   cidr           = "10.0.0.0/16"
   azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
